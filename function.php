9bb1a9da085deeb9d56b2cb8f455042f239cc8cd
MoussaBch <bourichmouss@gmail.com>
2017-05-09 14:40:02 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,9 +1,22 @@
-function VerificationInscription($mail)
+<?php
+function createUser()
 {
-    return 0;
+  $sql = $dbh->prepare("insert into users values ('',:mail,:password")) ;
+  $sql->bindParam(':mail', $_POST['mail'],PDO::PARAM_STR);
+  $sql->bindParam(':password', $_POST['password'],PDO::PARAM_STR);
+  $sql->execute();
 }
-
-function add_user($prenom, $nom, $mail, $password, $age)
+function deleteUser($id)
 {
-
+  $sql = $dbh->prepare("delete * from users where id= :id");
+  $sql->bindParam(':id', $id,PDO::PARAM_STR);
+  $sql->execute();
+}
+function updateUser($id)
+{
+  $sql = $dbh->prepare("update users set email = :mail, name= :name where id = :id");
+  $sql->bindParam(':mail', $_POST['email'],PDO::PARAM_STR);
+  $sql->bindParam(':name', $_POST['name'],PDO::PARAM_STR);
+  $sql->bindParam(':id', $id,PDO::PARAM_STR);
+  $sql->execute();
 }
