75dcde7591b937dda067c9babd4dc5d13f080a4f
regfarad <regfaradpro@gmail.com>
2017-05-13 19:04:53 +0200

    fonction en commentaires de maniere temporaire

--- GetInscription.php
+++ GetInscription.php
@@ -1,4 +1,5 @@
 <?php
+
 // Afficher les erreurs à l'écran
 ini_set('display_errors', 1);
 // Enregistrer les erreurs dans un fichier de log
@@ -9,47 +10,33 @@ ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
 error_reporting(e_all);
 
 
-				if (isset($_POST['inscrire'])
-				{
-				
-				IF    (empty($_POST['nom']) or empty($_POST['prenom']) or empty($_POST['password']) or empty($_POST['mail']) or empty($_POST['age']))
-				{
-					  
-						
-					print "<div class='alert alert-danger'>
+if (isset($_POST['inscrire'])) {
+
+    IF (empty($_POST['nom']) or empty($_POST['prenom']) or empty($_POST['password']) or empty($_POST['mail']) or empty($_POST['age'])) {
+
+
+        print "<div class='alert alert-danger'>
                           <strong>Remplissez les champs !</strong>
                           </div>";
-				}
-					else
-					{
-	
- $firstnName=$_POST[prenom];
- $lastName=$_POST[nom];
- $email=$_POST[maill];
- $password=$_POST[pass];
- $birthday=$_POST[datenaiss];
+    } else {
 
-
-
-}
+        $firstnName = $_POST[prenom];
+        $lastName = $_POST[nom];
+        $email = $_POST[maill];
+        $password = $_POST[pass];
+        $birthday = $_POST[datenaiss];
+    }
 }
 $verification = verification_inscription($email);
 
-if ($verification)
-{
-	print "<div class='alert alert-danger'>
+if ($verification) {
+    print "<div class='alert alert-danger'>
                 <strong>Votre pseudo est déjà utilisé par un membre</strong>
                 </div>";
-}
-else
-{    
-	add_user($lastName, $firstnName, $email, $password, $birthday );
-	print "<div class='alert alert-success'>
+} else {
+    add_user($lastName, $firstnName, $email, $password, $birthday);
+    print "<div class='alert alert-success'>
                 <strong>Bienvenue $firstnName</strong>
                 </div>";
-	
-	
 }
-
-
- ?>
+?>
--- function.php
+++ function.php
@@ -1,19 +1,19 @@
 <?php
-function verification_inscription
-{
-  
-  //Nous allons faire ca par après 
-  retrun false;
-  
-}
-function add_user($lastname,$firstname,$email,$password,$birthday)
-{
-  $sql = $dbh->prepare("insert into users values ('',:LastName,:firstName,:email,:password,:birthday")) ;
-  $sql->bindParam(':email', $email,PDO::PARAM_STR);
-  $sql->bindParam(':password', $password,PDO::PARAM_STR);
-  $sql->bindParam(':lastName', $lastname,PDO::PARAM_STR);
-  $sql->bindParam(':firstnName', $firstname,PDO::PARAM_STR);
-  $sql->bindParam(':birthday', $birthday,PDO::PARAM_STR);
-  $sql->execute();
-}
+////function verification_inscription
+////{
+////  
+////  //Nous allons faire ca par après 
+////  retrun false;
+////  
+////}
+//function add_user($lastname,$firstname,$email,$password,$birthday)
+//{
+//  $sql = $dbh->prepare("insert into users values ('',:LastName,:firstName,:email,:password,:birthday")) ;
+//  $sql->bindParam(':email', $email,PDO::PARAM_STR);
+//  $sql->bindParam(':password', $password,PDO::PARAM_STR);
+//  $sql->bindParam(':lastName', $lastname,PDO::PARAM_STR);
+//  $sql->bindParam(':firstnName', $firstname,PDO::PARAM_STR);
+//  $sql->bindParam(':birthday', $birthday,PDO::PARAM_STR);
+//  $sql->execute();
+//}
  
