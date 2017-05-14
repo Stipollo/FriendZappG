d0b6cddf6c3366b3e9bd5e25d019fe551129dcd6
regfarad <regfaradpro@gmail.com>
2017-05-14 11:19:39 +0200

    functions verif inscription

--- connexion.php
+++ connexion.php
@@ -1,18 +1,22 @@
 <?php
-$host='friendsapp.ml:22';
-$user='user';
-$pass='Test1234.';
-$db='FriendsApp';
+
+$host = 'friendsapp.ml:22';
+$user = 'user';
+$pass = 'Test1234.';
+$db = 'FriendsApp';
 $dsn = "mysql:host=$host;dbname=$db";
+$dbh = null;
 //**************************************
 //* connexion Ã  mysql et Ã  la db      *
 //**************************************
-try 
-{
-	$dbh = new PDO($dsn, $user, $pass); //db handle
-} 
-catch (PDOException $e) 
-{
-	die( "Erreur ! : " . $e->getMessage() );
+try {
+    
+    $dbh = new PDO($dsn, $user, $pass); //db handle
+    $dbh->exec("SET CHARACTER SET utf8");
+    
+} catch (PDOException $e) {
+    
+    die("Erreur ! : " . $e->getMessage());
+    
 }
 ?>
