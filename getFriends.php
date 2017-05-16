e24c01195e3d14e988b3b5f46f0cf91b2104a90c
VladimirTulcinsky <vladimir.tul@gmail.com>
2017-05-16 09:08:39 +0200

    Update getFriends.php

--- getFriends.php
+++ getFriends.php
@@ -1,17 +1,23 @@
 <?php
+require('DBconnect.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-getFriends(1,'50 53.846','04 20.361');  
-  function getFriends($id,$lat,$lng)
-  {
-    $infoFriend->id = $id;
-	$infoFriend->latitude = $lat;
-	$infoFriend->longitude = $lng;
 
-	$JSONResult = json_encode($infoFriend);
-
-	echo $JSONResult;
-
-  }
-
+    $searchInDB = "SELECT * FROM geolocalisation";
+    $stmt = $dbh->prepare($searchInDB);
+    $stmt->execute();
+    $data = array();
+    while($row = $stmt->fetch_assoc())
+	  {
+       $data_item['lat'] = $row['lat'];
+       
+    }
+    $encodedData = json_encode($data_item);
+    echo $encodedData;
+    exit;
 
 ?>
